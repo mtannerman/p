f9bcff23905324c0cb64de02563ae2371306c2c3
Mate Timar <timarmatealpine@gmail.com>
2017-12-05 00:14:21 +0100

    wip

--- csvparser/inc/csvparser/csvparser.h
+++ csvparser/inc/csvparser/csvparser.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <vector>
+#include <string>
+#include <memory>
+#include <istream>
+
+class CSVParser
+{
+public:
+    CSVParser(std::istream& istream, const char delimiter = ',', const char quotationMark = '\"');
+    bool readRecord(std::vector<std::string>& record);
+private:
+
+};
--- csvparser/src/csvparser.cpp
+++ csvparser/src/csvparser.cpp
@@ -1,1 +1,10 @@
 #include "csvparser/csvparser.h"
+
+
+CSVParser::CSVParser(std::istream& istream, const char delimiter, const char quotationMark)
+{}
+
+bool CSVParser::readRecord(std::vector<std::string>& record)
+{
+    return true;
+}
